9e253fc7510a4ed59898b643881be3f9b985792d
Carolyn Garvin <ctgarvin@iu.edu>
2020-08-05 23:54:35 -0400

    test

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ ax.set_xlim(0, 1), ax.set_xticks([])
 ax.set_ylim(0, 1), ax.set_yticks([])
 
 #Dots data
-num_dots = 10000
+num_dots = 1000
 dots = np.zeros(num_dots, dtype=[('position', float, 2),
                                  ('size',     float, 1),
                                  ('red_color', float, 4),
                                  ('green_color', float, 4),
-                                 ('blank', float, 4),
+                                 ('blank_color', float, 4),
                                  ('edge',    float, 4)])
 
 #Random position for new dots
@@ -28,30 +28,30 @@ scatter = ax.scatter(dots['position'][:, 0], dots['position'][:, 1],
 
 def update(frame_number):
     current_index = frame_number
+    blank_dot = current_index - 2
 
     # New dots position
     dots['position'][current_index] = np.random.uniform(0, 1, 2)
     dots['size'][current_index] = 5
     dots['edge'][current_index] = (0, 0, 0, 1)
+    dots['green_color'][current_index] = (0, 1, 0, 1)  # will always be the current index
+    #dots['red_color'][red_dot] = (1, 0, 0, 1)  # current - 1
+   # dots['blank_color'][blank_dot] = (0, 0, 0, 0)  # current - 2
 
-    # Update new dots
+
+    #Update new dots
     scatter.set_edgecolors(dots['edge'])
     scatter.set_sizes(dots['size'])
     scatter.set_offsets(dots['position'])
+    scatter.set_facecolor(dots['green_color'])
 
-    if (current_index == 0 or current_index == 1):
-        dots['green_color'][current_index] = (0, 1, 0, 1)
-        print(current_index)
-        scatter.set_facecolor(dots['green_color'])
-
-    if (current_index == 2):
-        print(current_index)
-        dots['red_color'][current_index] = (1, 0, 0, 1)
+    if(current_index > 0):
+        red_dot = current_index - 1
+        dots['red_color'][red_dot] = (1, 0, 0, 1)  # current - 1
         scatter.set_facecolor(dots['red_color'])
 
-    if(current_index > 2):
-        dots['blank'][current_index] = (0, 0, 0, 0)
-        scatter.set_facecolor(dots['blank'])
+        dots['green_color'][current_index] = (0, 1, 0, 1)
+        scatter.set_facecolor(dots['green_color'])
 
 
 animation = FuncAnimation(fig, update, interval=1000)
